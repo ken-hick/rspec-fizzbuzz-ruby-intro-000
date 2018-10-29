841f6ef033d295cadf921711f906458923339744
Ken Hick <hick.ken@gmail.com>
2018-10-29 17:37:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
+def fizzbuzz(int)
+  If int % 3 == 0
+  "Fizz"
+  If int % 5 == 0
+  "Buzz"
+  If int % 15 == 0
+  "FizzBuzz"
+  Else 
+  "nil"
 end
